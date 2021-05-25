78d2348a699afd2de9fb8abd3563c0287473e5c1
Reti35 <reneribera3@gmail.com>
2021-05-25 11:40:46 +0200

    Añado la clase conexion

---
+++ Programacion/Ukiku/src/conectarBd/Conexion.java
@@ -0,0 +1,58 @@
+package conectarBd;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Conexion {
+
+	private Connection conexion;
+	private static final String driver = "com.mysql.jdbc.Driver";
+	private static final String user = "root";
+	private static final String password = "";
+	private static final String url = "jdbc:mysql://localhost:3306/admin";
+	public Statement s;
+	public ResultSet rs;
+
+	public Conexion() {
+		
+		conexion = null;
+
+		try {
+
+			Class.forName(driver);
+			conexion = DriverManager.getConnection(url, user, password);
+
+			s = (Statement) conexion.createStatement();
+
+		} catch (ClassNotFoundException | SQLException e) {
+
+			e.printStackTrace();
+
+		}
+		
+	}
+
+	public Connection getConexion() {
+		
+		return conexion;
+		
+	}
+
+	public void desconectar() {
+		
+		try {
+			
+			conexion.close();
+			
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+			
+		}
+		
+	}
+	
+}
