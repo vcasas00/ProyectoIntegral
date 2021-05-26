aaab6f373beafb4d68619677212d295aca6ba672
Reti35 <reneribera3@gmail.com>
2021-05-26 10:37:58 +0200

    Termino la clase ukiku

--- Programacion/Ukiku/src/clases/Ukiku.java
+++ Programacion/Ukiku/src/clases/Ukiku.java
@@ -12,5 +12,46 @@ public class Ukiku extends Usuario{
 	
 	private ArrayList<Ciente> listarClientes;
 	private ArrayList<Producto> listarProducto;
+	private ArrayList<Proveedor> listarProveedor;
+	
+	public Ukiku() {
+		
+	}
+	
+	public ArrayList<Ciente> getListarClientes() {
+		
+		return listarClientes;
+		
+	}
+	public void setListarClientes(ArrayList<Ciente> listarClientes) {
+		
+		this.listarClientes = listarClientes;
+		
+	}
+	
+	public ArrayList<Producto> getListarProducto() {
+		
+		return listarProducto;
+		
+	}
+	
+	public void setListarProducto(ArrayList<Producto> listarProducto) {
+		
+		this.listarProducto = listarProducto;
+		
+	}
+	
+	public ArrayList<Proveedor> getListarProveedor() {
+		
+		return listarProveedor;
+		
+	}
+	
+	public void setListarProveedor(ArrayList<Proveedor> listarProveedor) {
+		
+		this.listarProveedor = listarProveedor;
+		
+	}
 
+	
 }
