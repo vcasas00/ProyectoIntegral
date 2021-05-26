9127f431efae03de79483446ca83199ff6aea2f0
Joaquin0-0 <72852313+Joaquin0-0@users.noreply.github.com>
2021-05-26 11:24:52 +0200

    base

---
+++ Base de Datos/ukiku_bd.sql
@@ -0,0 +1,137 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 24-05-2021 a las 13:27:37
+-- Versión del servidor: 10.4.18-MariaDB
+-- Versión de PHP: 7.2.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `ukiku_bd`
+--
+CREATE DATABASE IF NOT EXISTS `ukiku_bd` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci;
+USE `ukiku_bd`;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `Nombre` text COLLATE utf8mb4_spanish_ci NOT NULL,
+  `Apellido` text COLLATE utf8mb4_spanish_ci NOT NULL,
+  `Dni` varchar(8) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `Dirección` varchar(50) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `Teléfono` int(12) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `producto`
+--
+
+CREATE TABLE `producto` (
+  `Nombre` text COLLATE utf8mb4_spanish_ci NOT NULL,
+  `Codigo` int(10) NOT NULL,
+  `Categoría` varchar(50) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `Precio` double NOT NULL,
+  `Stock` int(11) NOT NULL,
+  `Cif_proveedor` varchar(12) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `dni_cliente` varchar(8) COLLATE utf8mb4_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `proveedor`
+--
+
+CREATE TABLE `proveedor` (
+  `Nombre` text COLLATE utf8mb4_spanish_ci NOT NULL,
+  `Cif` varchar(12) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `Producto` text COLLATE utf8mb4_spanish_ci NOT NULL,
+  `Dirección` varchar(50) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `Teléfono` int(12) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `id` int(11) NOT NULL,
+  `Nombre` text COLLATE utf8mb4_spanish_ci NOT NULL,
+  `Contraseña` varchar(12) COLLATE utf8mb4_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`Dni`);
+
+--
+-- Indices de la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD PRIMARY KEY (`Codigo`),
+  ADD KEY `Provee` (`Cif_proveedor`),
+  ADD KEY `Comipara` (`dni_cliente`);
+
+--
+-- Indices de la tabla `proveedor`
+--
+ALTER TABLE `proveedor`
+  ADD PRIMARY KEY (`Cif`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD CONSTRAINT `Comipara` FOREIGN KEY (`dni_cliente`) REFERENCES `cliente` (`Dni`),
+  ADD CONSTRAINT `Provee` FOREIGN KEY (`Cif_proveedor`) REFERENCES `proveedor` (`Cif`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
