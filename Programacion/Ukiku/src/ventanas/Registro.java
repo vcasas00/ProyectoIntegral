9d70069108478f11e842cb1cfa06f5670eec88d6
Reti35 <reneribera3@gmail.com>
2021-05-27 09:22:43 +0200

    Mejoro el diseño de la ventana

--- Programacion/Ukiku/src/ventanas/Registro.java
+++ Programacion/Ukiku/src/ventanas/Registro.java
@@ -3,12 +3,9 @@ package ventanas;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-
 import clases.Usuario;
-
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
-
 import java.awt.Font;
 import javax.swing.SwingConstants;
 import javax.swing.JTextField;
@@ -16,7 +13,9 @@ import javax.swing.JPasswordField;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-
+import java.awt.Color;
+import javax.swing.ImageIcon;
+import java.awt.Toolkit;
 /**
  * 
  * @author René, Vicent, Joaquín
@@ -32,9 +31,10 @@ public class Registro extends JFrame {
 	private JTextField textFieldnombre;
 	private JTextField textFieldContraseña;
 	private JLabel lblUkiku;
+	private JButton btnRegistrarse;
 	private JLabel lblUsuario;
 	private JLabel lblContrasea;
-	private JButton btnRegistrarse;
+	private JLabel lblIntroduceUnNombre;
 
 	/**
 	 * Launch the application.
@@ -50,49 +50,68 @@ public class Registro extends JFrame {
 	 * Create the frame.
 	 */
 	public Registro() {
+		setIconImage(Toolkit.getDefaultToolkit().getImage(Registro.class.getResource("/img/logorene.png")));
+		setBackground(Color.CYAN);
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setBounds(100, 100, 356, 300);
+		setBounds(100, 100, 403, 300);
 		contentPane = new JPanel();
+		contentPane.setBackground(Color.LIGHT_GRAY);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		lblUkiku = new JLabel("Ukiku");
+		lblUkiku = new JLabel("");
+		lblUkiku.setIcon(new ImageIcon(Registro.class.getResource("/img/xlarge_ukiku.png")));
 		lblUkiku.setHorizontalAlignment(SwingConstants.CENTER);
 		lblUkiku.setFont(new Font("Yu Gothic", Font.BOLD, 30));
-		lblUkiku.setBounds(112, 27, 112, 48);
+		lblUkiku.setBounds(0, 0, 388, 75);
 		contentPane.add(lblUkiku);
-		
-		lblUsuario = new JLabel("Usuario");
-		lblUsuario.setFont(new Font("Times New Roman", Font.PLAIN, 14));
-		lblUsuario.setBounds(10, 88, 46, 14);
-		contentPane.add(lblUsuario);
-		
-		lblContrasea = new JLabel("Contrase\u00F1a");
-		lblContrasea.setFont(new Font("Times New Roman", Font.PLAIN, 14));
-		lblContrasea.setBounds(10, 129, 73, 14);
-		contentPane.add(lblContrasea);
 		
 		textFieldnombre = new JTextField();
 		textFieldnombre.setColumns(10);
-		textFieldnombre.setBounds(91, 86, 144, 20);
+		textFieldnombre.setBounds(138, 125, 144, 20);
 		contentPane.add(textFieldnombre);
 		
 		textFieldContraseña = new JPasswordField();
 		textFieldContraseña.setColumns(10);
-		textFieldContraseña.setBounds(93, 127, 144, 20);
+		textFieldContraseña.setBounds(138, 156, 144, 20);
 		contentPane.add(textFieldContraseña);
 		
 		btnRegistrarse = new JButton("Registrarse");
+		btnRegistrarse.setForeground(new Color(255, 255, 255));
+		btnRegistrarse.setBackground(new Color(128, 128, 128));
 		btnRegistrarse.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				Usuario u = new Usuario();
-				u.registrarse(textFieldnombre.getText(), textFieldContraseña.getText());
-				JOptionPane.showMessageDialog(rootPane, "Añadido correctamente");
+				try {
+					Usuario u = new Usuario();
+					u.registrarse(textFieldnombre.getText(), textFieldContraseña.getText());
+					JOptionPane.showMessageDialog(rootPane, "Añadido correctamente");
+				} catch (Exception e) {
+					JOptionPane.showMessageDialog(rootPane, "Error al registrar el usuario");
+					e.printStackTrace();
+				}
 			}
 		});
-		btnRegistrarse.setBounds(112, 191, 106, 23);
+		btnRegistrarse.setBounds(138, 227, 144, 23);
 		contentPane.add(btnRegistrarse);
+		
+		lblUsuario = new JLabel("Usuario:");
+		lblUsuario.setFont(new Font("Times New Roman", Font.BOLD, 20));
+		lblUsuario.setBackground(Color.GRAY);
+		lblUsuario.setBounds(41, 121, 73, 23);
+		contentPane.add(lblUsuario);
+		
+		lblContrasea = new JLabel("Contrase\u00F1a:");
+		lblContrasea.setFont(new Font("Times New Roman", Font.BOLD, 20));
+		lblContrasea.setBackground(Color.GRAY);
+		lblContrasea.setBounds(10, 156, 104, 23);
+		contentPane.add(lblContrasea);
+		
+		lblIntroduceUnNombre = new JLabel("Introduce un nombre de usuario y contrase\u00F1a");
+		lblIntroduceUnNombre.setHorizontalAlignment(SwingConstants.CENTER);
+		lblIntroduceUnNombre.setFont(new Font("Times New Roman", Font.BOLD, 15));
+		lblIntroduceUnNombre.setBounds(37, 87, 327, 23);
+		contentPane.add(lblIntroduceUnNombre);
 		
 	}
 }
--- Programacion/Ukiku/src/ventanas/VentanaLogin.java
+++ Programacion/Ukiku/src/ventanas/VentanaLogin.java
@@ -20,7 +20,7 @@ import java.awt.Color;
 import java.awt.Toolkit;
 
 public class VentanaLogin extends JFrame {
-	
+
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private JTextField textFieldUsuario;
@@ -31,6 +31,7 @@ public class VentanaLogin extends JFrame {
 	private JLabel lblSiNoEstas;
 	private JLabel lblNewLabel;
 	private JLabel lblContrasea;
+	private JLabel lblInicioDeSesion;
 
 	/**
 	 * Launch the application.
@@ -46,72 +47,85 @@ public class VentanaLogin extends JFrame {
 	 * Create the frame.
 	 */
 	public VentanaLogin() {
-		setIconImage(Toolkit.getDefaultToolkit().getImage("K:\\PI\\Programacion\\Ukiku\\src\\img\\ukikulogo.png"));
+		setResizable(false);
+		setIconImage(Toolkit.getDefaultToolkit().getImage(VentanaLogin.class.getResource("/img/logorene.png")));
+		setBackground(Color.CYAN);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 415, 361);
 		contentPane = new JPanel();
+		contentPane.setBackground(Color.LIGHT_GRAY);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 
 		btnLogin = new JButton("Login");
+		btnLogin.setForeground(new Color(255, 255, 255));
+		btnLogin.setBackground(new Color(128, 128, 128));
 		btnLogin.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				// TODO funcionalidad login
-			Usuario u = new Usuario();
-			
-			if(u.acceder(textFieldUsuario.getText(), textFieldContraseña.getText())) {
-				JOptionPane.showMessageDialog(rootPane, "Login Correcto");
-			} else {
-				JOptionPane.showMessageDialog(rootPane, "Login Incorrecto");
-			}
-				
+				Usuario u = new Usuario();
+
+				if (u.acceder(textFieldUsuario.getText(), textFieldContraseña.getText())) {
+					JOptionPane.showMessageDialog(rootPane, "Login Correcto");
+				} else {
+					JOptionPane.showMessageDialog(rootPane, "Login Incorrecto");
+				}
+
 			}
 		});
-		btnLogin.setBounds(148, 183, 104, 23);
+		btnLogin.setBounds(138, 205, 144, 23);
 		contentPane.add(btnLogin);
 
 		btnRegistrarse = new JButton("Registrarse");
+		btnRegistrarse.setForeground(new Color(255, 255, 255));
+		btnRegistrarse.setBackground(new Color(128, 128, 128));
 		btnRegistrarse.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				Registro r = new Registro();
 				r.setVisible(true);
 			}
 		});
-		btnRegistrarse.setBounds(148, 261, 104, 23);
+		btnRegistrarse.setBounds(138, 288, 144, 23);
 		contentPane.add(btnRegistrarse);
 
 		textFieldUsuario = new JTextField();
-		textFieldUsuario.setBounds(138, 115, 144, 20);
+		textFieldUsuario.setBounds(138, 146, 144, 20);
 		contentPane.add(textFieldUsuario);
 		textFieldUsuario.setColumns(10);
 
 		textFieldContraseña = new JPasswordField();
-		textFieldContraseña.setBounds(138, 152, 144, 20);
+		textFieldContraseña.setBounds(138, 177, 144, 20);
 		contentPane.add(textFieldContraseña);
 		textFieldContraseña.setColumns(10);
 
 		lblUsuario = new JLabel("Usuario:");
 		lblUsuario.setBackground(Color.GRAY);
 		lblUsuario.setFont(new Font("Times New Roman", Font.BOLD, 20));
-		lblUsuario.setBounds(10, 111, 73, 23);
+		lblUsuario.setBounds(55, 142, 73, 23);
 		contentPane.add(lblUsuario);
-		
+
 		lblSiNoEstas = new JLabel("Si no estas registrado... \u00A1Reg\u00EDstrate!");
 		lblSiNoEstas.setFont(new Font("Times New Roman", Font.BOLD, 20));
 		lblSiNoEstas.setHorizontalAlignment(SwingConstants.CENTER);
-		lblSiNoEstas.setBounds(47, 227, 323, 23);
+		lblSiNoEstas.setBounds(55, 242, 323, 23);
 		contentPane.add(lblSiNoEstas);
-		
+
 		lblContrasea = new JLabel("Contrase\u00F1a:");
 		lblContrasea.setFont(new Font("Times New Roman", Font.BOLD, 20));
 		lblContrasea.setBackground(Color.GRAY);
-		lblContrasea.setBounds(10, 152, 104, 23);
+		lblContrasea.setBounds(24, 177, 104, 23);
 		contentPane.add(lblContrasea);
-		
+
 		lblNewLabel = new JLabel("");
-		lblNewLabel.setIcon(new ImageIcon("K:\\PI\\Programacion\\Ukiku\\src\\img\\xlarge_ukiku.png"));
+		lblNewLabel.setIcon(new ImageIcon(VentanaLogin.class.getResource("/img/xlarge_ukiku.png")));
 		lblNewLabel.setBounds(21, 0, 349, 107);
 		contentPane.add(lblNewLabel);
+
+		lblInicioDeSesion = new JLabel("Inicio de sesion");
+		lblInicioDeSesion.setHorizontalAlignment(SwingConstants.CENTER);
+		lblInicioDeSesion.setFont(new Font("Times New Roman", Font.BOLD, 20));
+		lblInicioDeSesion.setBounds(47, 108, 323, 23);
+		contentPane.add(lblInicioDeSesion);
 	}
 }
